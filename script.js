d83f4f6f3bfb091a0728bdb7715e9b4f66ea1ce4
Shavkatbek <shavkatbekmaxammadiyev@gmail.com>
2022-08-19 23:56:49 +0500

    salom  web devaloper

---
+++ script.js
@@ -0,0 +1,83 @@
+document.querySelector('#btn').addEventListener('click',function(){
+    myText()
+    myPass()
+})
+
+function myText() {
+    let message , text;
+    let errElement = document.getElementById('text');
+    message = document.getElementById('err');
+
+    text = document.getElementById('text').value;
+
+    try{
+        if(text == ""){
+            errElement.classList.remove('valid')
+            errElement.classList.add('err');
+            throw 'bo\'sh'
+        }
+        if(text.length > 0){
+            errElement.classList.remove('err')
+            errElement.classList.add('valid')
+            message.innerHTML
+        }
+    }catch(e){
+        message.innerHTML = 'qiymat' + e;
+    }
+}
+
+
+function myPass() {
+    let message , pass;
+    let errElement = document.getElementById('password');
+    message = document.getElementById('err2');
+    pass = document.getElementById('password').value;
+
+    try{
+        if(pass == ""){
+            errElement.classList.remove('valid')
+            errElement.classList.add('err');
+            throw 'bo\'sh'
+        }
+        if(pass.length > 0){
+            errElement.classList.remove('err')
+            errElement.classList.add('valid')
+            message.innerHTML = '';
+        }
+    }catch(e) {
+        message.innerHTML = 'qiymat' + e;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
